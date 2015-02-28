6a0cc4aca089d022e2918c31408a541bd53c15a1
F?bio Valentim <maitabom@gmail.com>
2015-02-28 13:52:09 -0300

    - Adi??o de t?tulos na pesquisa de lista de usu?rio. - Cria??o das novas actions do usu?rio.

--- app/Controller/UsuarioController.php
+++ app/Controller/UsuarioController.php
@@ -9,11 +9,22 @@ App::uses('AppController', 'Controller');
 class UsuarioController extends AppController {
 
     public function index() {
-
+        $title = "Lista de Usuários";
+        $this->set("title_for_layout", $title);
     }
 
     public function perfil($user) {
         $this->set("usuario", $user);
+    }
+
+    public function add() {
+        $this->redirect(array("action" => "cadastro", 0));
+    }
+
+    public function cadastro($id) {
+        $title = ($id > 0) ? "Edição do Usuário" : "Novo Usuário";
+        $this->set("title_for_layout", $title);
+        $this->set("id_usuario", $id);
     }
 
 }
