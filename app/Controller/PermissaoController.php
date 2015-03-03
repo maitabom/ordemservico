27a6173262d2641f8ad18802ee67cd258b8308ca
F?bio Valentim <maitabom@gmail.com>
2015-03-03 01:33:49 -0300

    Cria??o do controle de permiss?o

---
+++ app/Controller/PermissaoController.php
@@ -0,0 +1,15 @@
+<?php
+
+App::uses('AppController', 'Controller');
+
+/**
+ * CakePHP PermissaoController
+ * @author valentim
+ */
+class PermissaoController extends AppController {
+
+    public function index() {
+
+    }
+
+}
