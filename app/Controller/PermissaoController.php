533719b913eccb1fefe1d82ac5a7584d75e6c237
F?bio Valentim <maitabom@gmail.com>
2015-03-04 18:57:26 -0300

    Cria??o de a??es para controle de permiss?o.

--- app/Controller/PermissaoController.php
+++ app/Controller/PermissaoController.php
@@ -12,4 +12,18 @@ class PermissaoController extends AppController {
 
     }
 
+    public function add() {
+        $this->redirect(array("action" => "cadastro", 0));
+    }
+
+    public function edit($id) {
+        $this->redirect(array("action" => "cadastro", $id));
+    }
+
+    public function cadastro($id) {
+        $title = ($id > 0) ? "Editar Permissão" : "Nova Permissão";
+        $this->set("title_for_layout", $title);
+        $this->set("id_usuario", $id);
+    }
+
 }
