b5562a9b8057005cbd50163df5c84014bd029d2a
F?bio Valentim <maitabom@gmail.com>
2015-03-04 20:44:54 -0300

    Cria??o do m?dulo de clientes.

---
+++ app/Controller/ClienteController.php
@@ -0,0 +1,15 @@
+<?php
+
+App::uses('AppController', 'Controller');
+
+/**
+ * CakePHP ClienteController
+ * @author valentim
+ */
+class ClienteController extends AppController {
+
+    public function index() {
+
+    }
+
+}
---
+++ app/Model/Cliente.php
@@ -0,0 +1,14 @@
+<?php
+
+App::uses('AppModel', 'Model');
+
+/**
+ * CakePHP Cliente
+ * @author valentim
+ */
+class Cliente extends AppModel {
+
+    public $name = "Cliente";
+    public $useTable = false;
+
+}
