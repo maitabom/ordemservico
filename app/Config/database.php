eec05b161e3b2b5915d7ad3dccd1c9b1023089e5
F?bio Valentim <maitabom@gmail.com>
2015-05-07 14:45:40 -0300

    Altera??o na configura??o no banco de dados.

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,25 +1,26 @@
 <?php
+
 class DATABASE_CONFIG {
 
-	public $default = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'root',
-		'password' => '123456',
-		'database' => 'ordem_servico',
-		'prefix' => '',
-		'encoding' => 'utf8'
-	);
+    public $default = array(
+        'datasource' => 'Database/Mysql',
+        'persistent' => false,
+        'host' => 'localhost',
+        'login' => 'root',
+        'password' => '123456',
+        'database' => 'ordemservico',
+        'prefix' => '',
+        'encoding' => 'utf8'
+    );
+    public $test = array(
+        'datasource' => 'Database/Mysql',
+        'persistent' => false,
+        'host' => 'localhost',
+        'login' => 'user',
+        'password' => '',
+        'database' => 'database_name',
+        'prefix' => '',
+        'encoding' => 'utf8'
+    );
 
-	public $test = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'user',
-		'password' => '',
-		'database' => 'database_name',
-		'prefix' => '',
-		'encoding' => 'utf8'
-	);
 }
