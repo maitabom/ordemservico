49f878cc756a759c65015bf391b15b9b951d5bf1
F?bio Valentim <maitabom@gmail.com>
2015-05-07 18:38:10 -0300

    Cria??o do compomente Geografia

---
+++ app/Controller/Component/GeoComponent.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Componente de auxílio e fornecimento de informações geográficas gerais.
+ * @author valentim
+ */
+class GeoComponent extends Component {
+
+    public $components = array();
+
+    /**
+     * Retorna uma lista de unidades federativas do Brasil.
+     * @return array Lista de estados
+     */
+    public function listaUf() {
+
+        $estados = array(
+            "AC" => "Acre",
+            "AL" => "Alagoas",
+            "AM" => "Amazonas",
+            "AP" => "Amapá",
+            "BA" => "Bahia",
+            "CE" => "Ceará",
+            "DF" => "Distrito Federal",
+            "ES" => "Espírito Santo",
+            "GO" => "Goiás",
+            "MA" => "Maranhão",
+            "MT" => "Mato Grosso",
+            "MS" => "Mato Grosso do Sul",
+            "MG" => "Minas Gerais",
+            "PA" => "Pará",
+            "PB" => "Paraíba",
+            "PR" => "Paraná",
+            "PE" => "Pernambuco",
+            "PI" => "Piauí",
+            "RJ" => "Rio de Janeiro",
+            "RN" => "Rio Grande do Norte",
+            "RS" => "Rio Grande do Sul",
+            "RO" => "Rondônia",
+            "RR" => "Roraima",
+            "SC" => "Santa Catarina",
+            "SP" => "São Paulo",
+            "SE" => "Sergipe",
+            "TO" => "Tocantins"
+        );
+
+        return $estados;
+    }
+
+}
