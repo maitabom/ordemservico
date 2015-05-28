30bb0eb6c4352b821a37e2fa46256e6244e9faaa
F?bio Valentim <maitabom@gmail.com>
2015-05-28 17:58:41 -0300

    Adi??o de busca de fun??es da permiss?o.

--- app/Controller/PermissaoController.php
+++ app/Controller/PermissaoController.php
@@ -11,6 +11,7 @@ class PermissaoController extends AppController {
     public function beforeFilter() {
         $this->loadModel('Usuario');
         $this->loadModel('Permissao');
+        $this->loadModel('Funcao');
         parent::beforeFilter();
     }
 
@@ -28,8 +29,11 @@ class PermissaoController extends AppController {
 
     public function cadastro($id) {
         $title = ($id > 0) ? "Editar Permissão" : "Nova Permissão";
+        $funcoes = $this->Funcao->find('all', array('fields' => array('nome', 'chave')));
+
         $this->set("title_for_layout", $title);
         $this->set("id_usuario", $id);
+        $this->set("funcoes", $funcoes);
     }
 
 }
