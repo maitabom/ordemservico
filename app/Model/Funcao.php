7ac31c9dc2ab9a5150a754f20bdfd1f97f40f067
F?bio Valentim <maitabom@gmail.com>
2015-05-28 17:58:16 -0300

    Cria??o do modelo de fun??o

--- app/Model/Funcao.php
+++ app/Model/Funcao.php
@@ -10,5 +10,13 @@ class Funcao extends AppModel {
 
     public $name = "Funcao";
     public $useTable = "funcoes";
+    public $hasAndBelongsToMany = array(
+        "Permissao" => array(
+            "className" => "Permissao",
+            "joinTable" => "funcoes_grupos",
+            "foreignKey" => "grupos_id",
+            "associationForeignKey" => "funcoes_id"
+        )
+    );
 
 }
