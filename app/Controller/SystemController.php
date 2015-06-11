018ca5ca61e5bb9e6a0b44cce115d59614ad7463
F?bio Valentim <maitabom@gmail.com>
2015-06-11 15:42:01 -0300

    Implementa??o do recurso de efetuar logon e logoff do sistema.

--- app/Controller/SystemController.php
+++ app/Controller/SystemController.php
@@ -8,18 +8,67 @@ App::uses('AppController', 'Controller');
  */
 class SystemController extends AppController {
 
+    public function beforeFilter() {
+        $this->loadModel('Usuario');
+        parent::beforeFilter();
+    }
+
     public function login() {
-        $title = "Controle de Acesso";
-        $this->layout = "empty";
-        $this->set("title_for_layout", $title);
+        if ($this->isAuthorized()) {
+            $this->redirect(array("action" => "board"));
+        } else {
+            $title = "Controle de Acesso";
+            $this->layout = "empty";
+            $this->set("title_for_layout", $title);
+        }
     }
 
     public function logon() {
-        $this->redirect(array("action" => "board"));
+        if ($this->request->is('post')) {
+            $login = $this->request->data["Usuario"]["Usuario"];
+            $senha = $this->request->data["Usuario"]["Senha"];
+
+            if ($login == "" || $senha == "") {
+                $this->redirectLoginError("É obrigatório informar os dados de acesso.");
+            } else {
+
+                $retorno = $this->Usuario->find("first", array(
+                    "conditions" => array(
+                        "Usuario.nickname" => $login,
+                        "Usuario.senha" => $senha
+                )));
+
+                if (!empty($retorno)) {
+                    if ($retorno["Usuario"]["ativo"] == false) {
+                        $this->redirectLoginError("O usuário encontra-se inativo para o sistema.");
+                    }
+
+                    $this->Session->write("UsuarioID", $retorno["Usuario"]["ID"]);
+                    $this->Session->write("UsuarioUsuario", $login);
+                    $this->Session->write("UsuarioEmail", $retorno["Usuario"]["email"]);
+                    $this->Session->write("UsuarioNome", $retorno["Usuario"]["nome"]);
+                    $this->Session->write("UsuarioCargo", $retorno["Usuario"]["cargo"]);
+                    $this->Session->write("Usuario", $retorno);
+
+                    if ($retorno["Usuario"]["verificar"] == true) {
+                        $this->redirect(array("action" => "password"));
+                    } else {
+                        $this->redirect(array("action" => "board"));
+                    }
+                } else {
+                    $this->redirectLoginError("Os dados de acesso estão invalidos.");
+                }
+            }
+        }
     }
 
     public function logoff() {
+        $this->Session->destroy();
         $this->redirect(array("action" => "login"));
+    }
+
+    public function password() {
+
     }
 
     public function board() {
