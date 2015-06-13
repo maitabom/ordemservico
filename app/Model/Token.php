49c97f8da03eeae12d628f293619f4adbabe01d7
F?bio Valentim <maitabom@gmail.com>
2015-06-13 14:48:00 -0300

    Cria??o do modelo de token do sistema.

---
+++ app/Model/Token.php
@@ -0,0 +1,14 @@
+<?php
+
+App::uses('AppModel', 'Model');
+
+/**
+ * CakePHP Token
+ * @author valentim
+ */
+class Token extends AppModel {
+
+    public $name = "Token";
+    public $useTable = "tokens";
+
+}
