31f5863daecfd3c845756b0cf2c535bf9c49b983
F?bio Valentim <maitabom@gmail.com>
2015-06-13 14:47:34 -0300

    Cria??o do controle de token.

---
+++ app/Controller/TokenController.php
@@ -0,0 +1,64 @@
+<?php
+
+App::uses('AppController', 'Controller');
+
+/**
+ * CakePHP TokenController
+ * @author valentim
+ */
+class TokenController extends AppController {
+
+    private $horaLimite = 48;
+
+    public function index($id) {
+
+        $this->layout = "empty";
+        $result = $this->Token->find("first", array(
+            "conditions" => array(
+                "Token.chave" => $id,
+                "Token.resolvido" => FALSE,
+                "Token.ativo" => TRUE
+            ),
+            "order" => array(
+                "Token.datacriacao DESC"
+            )
+        ));
+
+        if (empty($result)) {
+
+            $this->Session->setFlash("O token informado é inválido ou inexistente no sistema. Favor, repetir a operação.");
+        } else {
+
+            $dataCriacao = $result["Token"]["datacriacao"];
+            $intervalo = $this->Tokin->getInterval($dataCriacao);
+
+            var_dump($dataCriacao);
+            var_dump($intervalo);
+
+            if ($intervalo >= $this->horaLimite) {
+                $this->Session->setFlash("Infelizmente, o token informado perdeu a validade. Favor repetir a operação.");
+                $this->Tokin->markUnresolved($id);
+            } else {
+                $produto = $result["Token"]["produto"];
+                $idUsuario = $result["Token"]["usuario"];
+
+                $this->Tokin->markResolved($id);
+                $this->executarToken($produto, $idUsuario);
+            }
+        }
+    }
+
+    private function executarToken($produto, $idUsuario) {
+        switch ($produto) {
+
+            case "lembrarsenha":
+                $this->redirect(array("controller" => "system", "action" => "remember", base64_encode($idUsuario)));
+                break;
+
+            default:
+                $this->redirect(array("controller" => "system", "action" => "login"));
+                break;
+        }
+    }
+
+}
