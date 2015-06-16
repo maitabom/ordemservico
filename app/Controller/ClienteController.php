df05109afbcb7cc7ae5264bb35b9f45526d81b27
F?bio Valentim <maitabom@gmail.com>
2015-06-16 17:44:47 -0300

    Implementa??o dos recursos de inclus?o altera??o e lista de clientes.

--- app/Controller/ClienteController.php
+++ app/Controller/ClienteController.php
@@ -8,8 +8,17 @@ App::uses('AppController', 'Controller');
  */
 class ClienteController extends AppController {
 
-    public function index() {
+    public function beforeFilter() {
+        $this->loadModel('Cliente');
+        parent::beforeFilter();
+    }
 
+    public function index() {
+        $clientes = $this->Cliente->find('all');
+        $title = "Lista de Clientes";
+
+        $this->set("title_for_layout", $title);
+        $this->set("clientes", $clientes);
     }
 
     public function add() {
@@ -22,8 +31,41 @@ class ClienteController extends AppController {
 
     public function cadastro($id) {
         $title = ($id > 0) ? "Editar Cliente" : "Novo Cliente";
+        $estados = $this->Geo->listaUf();
+        $tipos_pessoas = ["F" => "Pessoa Física", "J" => "Pessoa Jurídica"];
+
+        if ($id > 0) {
+            $this->request->data = $this->Cliente->read(null, $id);
+        }
+
         $this->set("title_for_layout", $title);
         $this->set("id_cliente", $id);
+        $this->set("estados", $estados);
+        $this->set("tipos_cliente", $tipos_pessoas);
+    }
+
+    public function save() {
+        if ($this->request->is('post') || $this->request->is('put')) {
+            $data = $this->request->data;
+            $mensagem = "";
+
+            $data["Cliente"]["cep"] = $this->clearMask($data["Cliente"]["cep"]);
+            $data["Cliente"]["telefone"] = $this->clearMask($data["Cliente"]["telefone"]);
+            $data["Cliente"]["celular"] = $this->clearMask($data["Cliente"]["celular"]);
+            $data["Cliente"]["documento_fiscal"] = $this->clearMask($data["Cliente"]["documento_fiscal"]);
+
+            try {
+                $this->Cliente->save($data);
+                $this->Dialog->alert("O cliente foi salvo com sucesso!");
+
+                $id = $this->Cliente->id;
+                $this->redirect(array("action" => "cadastro", $id));
+            } catch (Exception $ex) {
+                $mensagem = "Ocorreu um erro no sistema ao salvar o cliente.";
+                $this->Dialog->error($mensagem, $ex->getMessage());
+                $this->redirect(array("action" => "cadastro", $data["Cliente"]["id"]));
+            }
+        }
     }
 
 }
