3da170cfa4874f87cd5dce20c44aaffb70d1f2ea
F?bio Valentim <maitabom@gmail.com>
2015-06-19 13:36:27 -0300

    Cria??o do modelo de equipamento

---
+++ app/Model/Equipamento.php
@@ -0,0 +1,14 @@
+<?php
+
+App::uses('AppModel', 'Model');
+
+/**
+ * CakePHP Equipamento
+ * @author valentim
+ */
+class Equipamento extends AppModel {
+
+    public $name = "Equipamento";
+    public $useTable = "equipamentos";
+
+}
