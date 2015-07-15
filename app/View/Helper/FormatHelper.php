6668115bd85eca0fe4fef79d4878c1b3e474b3e0
F?bio Valentim <maitabom@gmail.com>
2015-07-15 12:21:22 -0300

    Cria??o do componente de formata??o de valores.

---
+++ app/View/Helper/FormatHelper.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Classe de auxílio de manipulação de dados para formatação adequada ao usuário.
+ * @author valentim
+ */
+class FormatHelper extends AppHelper {
+
+    public $helpers = array();
+
+    public function __construct(View $View, $settings = array()) {
+        parent::__construct($View, $settings);
+    }
+
+    /**
+     * Formata para o número de telefone
+     * @param string $value Valor original do telefone
+     * @return string Telefone formatado
+     */
+    public function phone($value) {
+        $pattern = '/(\d{2})(\d{4})(\d*)/';
+        $result = preg_replace($pattern, '($1) $2-$3', $value);
+
+        return $result;
+    }
+
+    /**
+     * Formata um número colocando zeros a esquerda
+     * @param string $value O valor a ser formatado
+     * @param int $lenght A quantidade de zeros a ser adicionada. Por padrão, será 7.
+     * @return string O valor formatado.
+     */
+    public function zeroPad($value, $lenght = 7) {
+        return str_pad($value, $lenght, "0", STR_PAD_LEFT);
+    }
+
+}
