72ad398b23ebf418e44fcb27c9e5b38a59428a2c
F?bio Valentim <maitabom@gmail.com>
2015-07-15 17:56:38 -0300

    Melhoria simplificada da busca de banco de dados relacionados.

--- app/Model/OrdemServico.php
+++ app/Model/OrdemServico.php
@@ -10,5 +10,23 @@ class OrdemServico extends AppModel {
 
     public $name = "OrdemServico";
     public $useTable = "ordens_servico";
+    public $belongsTo = array(
+        "Cliente" => array(
+            "className" => "Cliente",
+            "foreignKey" => "id_cliente"
+        ),
+        "Responsavel" => array(
+            "className" => "Usuario",
+            "foreignKey" => "responsavel"
+        ),
+        "Equipamento" => array(
+            "className" => "Equipamento",
+            "foreignKey" => "equipamento"
+        ),
+        "ModoEntrega" => array(
+            "className" => "ModoEntrega",
+            "foreignKey" => "modo_entrega"
+        )
+    );
 
 }
