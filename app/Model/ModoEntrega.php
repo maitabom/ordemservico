986f7f7e659bf4b7d14aea168d5cda6ffc045924
F?bio Valentim <maitabom@gmail.com>
2015-07-14 23:00:14 -0300

    Cria??o do modelo de modo de entrega.

---
+++ app/Model/ModoEntrega.php
@@ -0,0 +1,14 @@
+<?php
+
+App::uses('AppModel', 'Model');
+
+/**
+ * CakePHP ModoEntrega
+ * @author valentim
+ */
+class ModoEntrega extends AppModel {
+
+    public $name = "ModoEntrega";
+    public $useTable = "modo_entrega";
+
+}
