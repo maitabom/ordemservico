6efdadfcdf1a5ee83d7bc207e8e67c872ee44ec1
F?bio Valentim <maitabom@gmail.com>
2015-08-12 18:07:30 -0300

    Cria??o do modelo para template de ordem de servi?o

---
+++ app/Model/OrdemServicoModelo.php
@@ -0,0 +1,28 @@
+<?php
+
+App::uses('AppModel', 'Model');
+
+/**
+ * CakePHP OrdemServicoModelo
+ * @author valentim
+ */
+class OrdemServicoModelo extends AppModel {
+
+    public $name = "OrdemServicoModelo";
+    public $useTable = "ordem_servico_modelo";
+    public $belongsTo = array(
+        "Cliente" => array(
+            "className" => "Cliente",
+            "foreignKey" => "id_cliente"
+        ),
+        "Equipamento" => array(
+            "className" => "Equipamento",
+            "foreignKey" => "equipamento"
+        ),
+        "ModoEntrega" => array(
+            "className" => "ModoEntrega",
+            "foreignKey" => "modo_entrega"
+        )
+    );
+
+}
