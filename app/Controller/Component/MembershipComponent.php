7b75dee2f3bf6e63888ee29e6c9918935ec8f0e8
F?bio Valentim <maitabom@gmail.com>
2015-08-19 14:15:08 -0300

    Encapsulamento do m?todo de varredura de fun??es por URL

--- app/Controller/Component/MembershipComponent.php
+++ app/Controller/Component/MembershipComponent.php
@@ -9,44 +9,6 @@ class MembershipComponent extends Component {
     public $components = array("Session");
 
     /**
-     * Gera uma lista de funções por controllers e actions
-     * @return array Lista de funções por controllers e actions
-     */
-    public function actionRoles() {
-        $roles = [
-            "LISTA_USUARIOS" => ["controller" => "usuario", "action" => "index"],
-            "ADICIONAR_USUARIOS" => ["controller" => "usuario", "action" => "add"],
-            "EDITAR_USUARIOS" => ["controller" => "usuario", "action" => "edit"],
-            "EXCLUIR_USUARIOS" => ["controller" => "usuario", "action" => "delete"],
-            "LISTA_PERMISSOES" => ["controller" => "permissao", "action" => "index"],
-            "ADICIONAR_PERMISSOES" => ["controller" => "permissao", "action" => "add"],
-            "EDITAR_PERMISSOES" => ["controller" => "permissao", "action" => "edit"],
-            "EXCLUIR_PERMISSOES" => ["controller" => "permissao", "action" => "delete"],
-            "LISTA_CLIENTES" => ["controller" => "cliente", "action" => "index"],
-            "ADICIONAR_CLIENTES" => ["controller" => "cliente", "action" => "add"],
-            "EDITAR_CLIENTES" => ["controller" => "cliente", "action" => "edit"],
-            "EXCLUIR_CLIENTES" => ["controller" => "cliente", "action" => "delete"],
-            "LISTA_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "index"],
-            "ADICIONAR_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "add"],
-            "EDITAR_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "edit"],
-            "VISUALIZAR_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "documento"],
-            "CANCELAR_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "cancelar"],
-            "CONCLUIR_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "concluir"],
-            "LISTA_TAREFAS" => ["controller" => "ordem_servico", "action" => "prioridades"],
-            "LISTA_MODELO_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "templates"],
-            "CADASTRAR_MODELO_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "template_create"],
-            "EDITAR_MODELO_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "template_edit"],
-            "EXCLUIR_MODELO_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "delete"],
-            "LISTA_EQUIPAMENTOS" => ["controller" => "equipamento", "action" => "index"],
-            "CADASTRAR_EQUIPAMENTOS" => ["controller" => "equipamento", "action" => "add"],
-            "EDITAR_EQUIPAMENTOS" => ["controller" => "equipamento", "action" => "edit"],
-            "EXCLUIR_EQUIPAMENTOS" => ["controller" => "equipamento", "action" => "delete"]
-        ];
-
-        return $roles;
-    }
-
-    /**
      * Faz tratamento de permissões de tela para usuário, por meio de um controle ou de ação.
      * @param array $url Endereço por meio de controller e action informado.
      * @param mixed $userID ID ou nickname do usuário.
@@ -122,6 +84,44 @@ class MembershipComponent extends Component {
         $this->Session->write("USER_FUNCTIONS", $fs);
 
         return $fs;
+    }
+
+    /**
+     * Gera uma lista de funções por controllers e actions
+     * @return array Lista de funções por controllers e actions
+     */
+    private function actionRoles() {
+        $roles = [
+            "LISTA_USUARIOS" => ["controller" => "usuario", "action" => "index"],
+            "ADICIONAR_USUARIOS" => ["controller" => "usuario", "action" => "add"],
+            "EDITAR_USUARIOS" => ["controller" => "usuario", "action" => "edit"],
+            "EXCLUIR_USUARIOS" => ["controller" => "usuario", "action" => "delete"],
+            "LISTA_PERMISSOES" => ["controller" => "permissao", "action" => "index"],
+            "ADICIONAR_PERMISSOES" => ["controller" => "permissao", "action" => "add"],
+            "EDITAR_PERMISSOES" => ["controller" => "permissao", "action" => "edit"],
+            "EXCLUIR_PERMISSOES" => ["controller" => "permissao", "action" => "delete"],
+            "LISTA_CLIENTES" => ["controller" => "cliente", "action" => "index"],
+            "ADICIONAR_CLIENTES" => ["controller" => "cliente", "action" => "add"],
+            "EDITAR_CLIENTES" => ["controller" => "cliente", "action" => "edit"],
+            "EXCLUIR_CLIENTES" => ["controller" => "cliente", "action" => "delete"],
+            "LISTA_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "index"],
+            "ADICIONAR_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "add"],
+            "EDITAR_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "edit"],
+            "VISUALIZAR_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "documento"],
+            "CANCELAR_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "cancelar"],
+            "CONCLUIR_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "concluir"],
+            "LISTA_TAREFAS" => ["controller" => "ordem_servico", "action" => "prioridades"],
+            "LISTA_MODELO_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "templates"],
+            "CADASTRAR_MODELO_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "template_create"],
+            "EDITAR_MODELO_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "template_edit"],
+            "EXCLUIR_MODELO_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "delete"],
+            "LISTA_EQUIPAMENTOS" => ["controller" => "equipamento", "action" => "index"],
+            "CADASTRAR_EQUIPAMENTOS" => ["controller" => "equipamento", "action" => "add"],
+            "EDITAR_EQUIPAMENTOS" => ["controller" => "equipamento", "action" => "edit"],
+            "EXCLUIR_EQUIPAMENTOS" => ["controller" => "equipamento", "action" => "delete"]
+        ];
+
+        return $roles;
     }
 
 }
