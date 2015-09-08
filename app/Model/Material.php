c9074a26df98bec3747a591d11f5e4009a03d7a8
F?bio Valentim <maitabom@gmail.com>
2015-09-08 17:41:03 -0300

    Cria??o do modelo de material.

---
+++ app/Model/Material.php
@@ -0,0 +1,14 @@
+<?php
+
+App::uses('AppModel', 'Model');
+
+/**
+ * CakePHP Material
+ * @author valentim
+ */
+class Material extends AppModel {
+
+    public $name = "Material";
+    public $useTable = "materiais";
+
+}
