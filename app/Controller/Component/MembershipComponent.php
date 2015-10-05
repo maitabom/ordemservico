c37509a2a2fea53e8d9ca6b4932ca4e48d5ab77b
F?bio Valentim <maitabom@gmail.com>
2015-10-05 18:38:40 -0300

    Cria??o da metologia para permitir que certas fun??es se tornem p?blicas, sem a necessidade de valida??o de controle de acesso.

--- app/Controller/Component/MembershipComponent.php
+++ app/Controller/Component/MembershipComponent.php
@@ -20,13 +20,20 @@ class MembershipComponent extends Component {
 
         if ($funcao != null) {
 
-            $funcoes = ($this->Session->check("USER_FUNCTIONS")) ? $this->Session->read("USER_FUNCTIONS") : $this->getFunctionsUser($userID);
+            $role = $this->getRole($url);
 
-            foreach ($funcoes as $fn) {
-                if ($fn["fn"]["chave"] == $funcao) {
-                    $autorizado = true;
-                    break;
+            if (!$role[$funcao]["public"]) {
+
+                $funcoes = ($this->Session->check("USER_FUNCTIONS")) ? $this->Session->read("USER_FUNCTIONS") : $this->getFunctionsUser($userID);
+
+                foreach ($funcoes as $fn) {
+                    if ($fn["fn"]["chave"] == $funcao) {
+                        $autorizado = true;
+                        break;
+                    }
                 }
+            } else {
+                $autorizado = true;
             }
         } else {
             $autorizado = true;
@@ -36,8 +43,26 @@ class MembershipComponent extends Component {
     }
 
     /**
+     * Libera o acesso publico de uma determinada funçao.
+     * @param type $url Endereço por meio de controller e action informado.
+     * @return boolean Se o endereço e publico, retirando a necessidade de validaçao por sessao.
+     */
+    public function handlePublicRole($url) {
+        $function = $this->getFunction($url);
+        $role = $this->getRole($url);
+        $autorizado = false;
+
+        if ($role[$function]["public"]) {
+            $autorizado = true;
+        }
+
+        return $autorizado;
+    }
+
+    /**
      * Obtém a função por meio do endereço informado.
      * @param array $url Endereço por meio de controller e action informado.
+     * @param boolean $public Realiza a busca entre as funções públicas.
      * @return string Função relacionada ou nulo, caso não a encontre.
      */
     private function getFunction($url) {
@@ -47,10 +72,25 @@ class MembershipComponent extends Component {
         foreach ($roles as $key => $value) {
             if (($url["controller"] == $value["controller"]) && ($url["action"] == $value["action"])) {
                 $function = $key;
+                break;
             }
         }
 
         return $function;
+    }
+
+    private function getRole($url) {
+        $roles = $this->actionRoles();
+        $role = null;
+
+        foreach ($roles as $key => $value) {
+            if (($url["controller"] == $value["controller"]) && ($url["action"] == $value["action"])) {
+                $role = [$key => $value];
+                break;
+            }
+        }
+
+        return $role;
     }
 
     /**
@@ -92,37 +132,37 @@ class MembershipComponent extends Component {
      */
     private function actionRoles() {
         $roles = [
-            "LISTA_USUARIOS" => ["controller" => "usuario", "action" => "index"],
-            "ADICIONAR_USUARIOS" => ["controller" => "usuario", "action" => "add"],
-            "EDITAR_USUARIOS" => ["controller" => "usuario", "action" => "edit"],
-            "EXCLUIR_USUARIOS" => ["controller" => "usuario", "action" => "delete"],
-            "LISTA_PERMISSOES" => ["controller" => "permissao", "action" => "index"],
-            "ADICIONAR_PERMISSOES" => ["controller" => "permissao", "action" => "add"],
-            "EDITAR_PERMISSOES" => ["controller" => "permissao", "action" => "edit"],
-            "EXCLUIR_PERMISSOES" => ["controller" => "permissao", "action" => "delete"],
-            "LISTA_CLIENTES" => ["controller" => "cliente", "action" => "index"],
-            "ADICIONAR_CLIENTES" => ["controller" => "cliente", "action" => "add"],
-            "EDITAR_CLIENTES" => ["controller" => "cliente", "action" => "edit"],
-            "EXCLUIR_CLIENTES" => ["controller" => "cliente", "action" => "delete"],
-            "LISTA_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "index"],
-            "ADICIONAR_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "add"],
-            "EDITAR_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "edit"],
-            "VISUALIZAR_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "documento"],
-            "CANCELAR_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "cancelar"],
-            "CONCLUIR_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "concluir"],
-            "LISTA_TAREFAS" => ["controller" => "ordem_servico", "action" => "prioridades"],
-            "LISTA_MODELO_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "templates"],
-            "CADASTRAR_MODELO_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "template_create"],
-            "EDITAR_MODELO_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "template_edit"],
-            "EXCLUIR_MODELO_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "delete"],
-            "LISTA_EQUIPAMENTOS" => ["controller" => "equipamento", "action" => "index"],
-            "CADASTRAR_EQUIPAMENTOS" => ["controller" => "equipamento", "action" => "add"],
-            "EDITAR_EQUIPAMENTOS" => ["controller" => "equipamento", "action" => "edit"],
-            "EXCLUIR_EQUIPAMENTOS" => ["controller" => "equipamento", "action" => "delete"],
-            "LISTAR_MATERIAIS" => ["controller" => "material", "action" => "index"],
-            "CADASTRAR_MATERIAIS" => ["controller" => "material", "action" => "add"],
-            "EDITAR_MATERIAIS" => ["controller" => "material", "action" => "edit"],
-            "EXCLUIR_MATERIAIS" => ["controller" => "material", "action" => "delete"]
+            "LISTA_USUARIOS" => ["controller" => "usuario", "action" => "index", "public" => false],
+            "ADICIONAR_USUARIOS" => ["controller" => "usuario", "action" => "add", "public" => false],
+            "EDITAR_USUARIOS" => ["controller" => "usuario", "action" => "edit", "public" => false],
+            "EXCLUIR_USUARIOS" => ["controller" => "usuario", "action" => "delete", "public" => false],
+            "LISTA_PERMISSOES" => ["controller" => "permissao", "action" => "index", "public" => false],
+            "ADICIONAR_PERMISSOES" => ["controller" => "permissao", "action" => "add", "public" => false],
+            "EDITAR_PERMISSOES" => ["controller" => "permissao", "action" => "edit", "public" => false],
+            "EXCLUIR_PERMISSOES" => ["controller" => "permissao", "action" => "delete", "public" => false],
+            "LISTA_CLIENTES" => ["controller" => "cliente", "action" => "index", "public" => false],
+            "ADICIONAR_CLIENTES" => ["controller" => "cliente", "action" => "add", "public" => false],
+            "EDITAR_CLIENTES" => ["controller" => "cliente", "action" => "edit", "public" => false],
+            "EXCLUIR_CLIENTES" => ["controller" => "cliente", "action" => "delete", "public" => false],
+            "LISTA_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "index", "public" => false],
+            "ADICIONAR_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "add", "public" => false],
+            "EDITAR_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "edit", "public" => false],
+            "VISUALIZAR_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "documento", "public" => false],
+            "CANCELAR_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "cancelar", "public" => false],
+            "CONCLUIR_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "concluir", "public" => false],
+            "LISTA_TAREFAS" => ["controller" => "ordem_servico", "action" => "prioridades", "public" => true],
+            "LISTA_MODELO_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "templates", "public" => false],
+            "CADASTRAR_MODELO_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "template_create", "public" => false],
+            "EDITAR_MODELO_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "template_edit", "public" => false],
+            "EXCLUIR_MODELO_ORDEM_SERVICO" => ["controller" => "ordem_servico", "action" => "delete", "public" => false],
+            "LISTA_EQUIPAMENTOS" => ["controller" => "equipamento", "action" => "index", "public" => false],
+            "CADASTRAR_EQUIPAMENTOS" => ["controller" => "equipamento", "action" => "add", "public" => false],
+            "EDITAR_EQUIPAMENTOS" => ["controller" => "equipamento", "action" => "edit", "public" => false],
+            "EXCLUIR_EQUIPAMENTOS" => ["controller" => "equipamento", "action" => "delete", "public" => false],
+            "LISTAR_MATERIAIS" => ["controller" => "material", "action" => "index", "public" => false],
+            "CADASTRAR_MATERIAIS" => ["controller" => "material", "action" => "add", "public" => false],
+            "EDITAR_MATERIAIS" => ["controller" => "material", "action" => "edit", "public" => false],
+            "EXCLUIR_MATERIAIS" => ["controller" => "material", "action" => "delete", "public" => false]
         ];
 
         return $roles;
